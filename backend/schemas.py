71ad7d478c579c281906db258b5bad36a2294d4d
Luca Woik <luca@luca-woik.de>
2023-03-10 18:51:35 +0100

    Added pydantic model (schema) for 'users' table

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,3 +1,17 @@
 # Creating the pydantic Models
 
 from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    full_name: str
+    organisation_unit: str
+    has_admin_privileges: bool
+
+
+class UserCreate(UserBase):
+    hashed_password: str
+
+
+class User(UserBase):
+    rz_username: str
