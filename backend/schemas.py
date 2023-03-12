0840fa527e0181989a535b339e125a3073d623ad
Luca Woik <luca@luca-woik.de>
2023-03-12 15:01:59 +0100

    Added pydantic model base (schema) for all tables

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,15 +3,44 @@
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
+class User(BaseModel):
+    rz_username: str
     full_name: str
     organisation_unit: str
     has_admin_privileges: bool
-
-
-class UserCreate(UserBase):
     hashed_password: str
 
 
-class User(UserBase):
+class Device(BaseModel):
+    device_id: str
+    title: str
+    device_type: str
+    description: str
+    accessories: str
+    rz_username_buyer: str
+    serial_number: str
+    image_url: str
+
+
+class OwnerTransaction(BaseModel):
+    owner_transaction_id: str
     rz_username: str
+    timestamp_owner_since: str
+    device_id: str
+
+
+class LocationTransaction(BaseModel):
+    location_transaction_id: str
+    room_code: str
+    timestamp_located_since: str
+    device_id: str
+
+
+class PurchasingInformation(BaseModel):
+    purchasing_information_id: str
+    price: str
+    timestamp_warranty_end: str
+    timestamp_purchase: str
+    cost_centre: int
+    seller: str
+    device_id: str
