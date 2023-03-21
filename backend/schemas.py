6441b7b603d7e1667ca272b9a27720ac4f50e3da
Luca Woik <luca@luca-woik.de>
2023-03-21 12:32:23 +0100

    Added pydantic user model for OAuth2 implementation

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,3 +2,22 @@
 
 from pydantic import BaseModel
 
+
+# TODO: Add source? (https://fastapi.tiangolo.com/tutorial/security/get-current-user/#__tabbed_2_1)
+class User(BaseModel):
+    """ The current pydantic user schema according to the FastAPI Docs
+        - Currently using a fake_hashed password!!!
+    Attributes:
+        rz_username:str
+        full_name : str
+        organisation_unit : str
+        has_admin_privileges : bool
+        hashed_password : str
+    """
+    rz_username: str
+    full_name: str
+    organisation_unit: str
+    has_admin_privileges: bool
+    # TODO: Currently using a fake hashed password instead of a securely hashed one
+    fake_hashed_password: str
+
