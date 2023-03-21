a7188ce23f56738060de0c518c27b3ed5c511a37
Luca Woik <luca@luca-woik.de>
2023-03-21 19:34:33 +0100

    New API Endpoint (/users/me) which returns the current user

--- backend/main.py
+++ backend/main.py
@@ -23,13 +23,22 @@ def get_db():
         db.close()
 
 
+# TODO: Current implementation according to FastAPI docs
+#  (https://fastapi.tiangolo.com/tutorial/security/get-current-user/#__tabbed_2_1)
+def fake_decode_token(token):
+    return schemas.User(
+        rz_username=token + "fakedecoded", full_name="User", organisation_unit="1", has_admin_privileges=True
+    )
+
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     """ Dependency, which returns the current user.
         - oauth2_scheme as sub-dependency
     :param token: str
     :return: current_user: User
     """
-
+    user = fake_decode_token(token)
+    return user
 
 
 @app.get("/")
@@ -40,3 +49,8 @@ def read_root():
 @app.get("/test/")
 async def testing_auth(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[schemas.User, Depends(get_current_user)]):
+    return current_user
