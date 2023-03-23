1cdc9dba8855c4c2dc06d0778d826697a83f6f64
Luca Woik <luca@luca-woik.de>
2023-03-23 21:49:50 +0100

    Restructured the pydantic models for User

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,25 +4,34 @@ from pydantic import BaseModel
 
 
 # TODO: Add source? (https://fastapi.tiangolo.com/tutorial/security/get-current-user/#__tabbed_2_1)
-class User(BaseModel):
+class UserBase(BaseModel):
+    """User BaseModel
+    Attributes:
+        rz_username: str
+    """
+    rz_username: str
+
+
+class UserCreate(UserBase):
+    """UserBase subclass for referencing the users password
+    Attributes:
+        hashed_password : str
+    """
+    hashed_password: str
+
+
+class User(UserBase):
     """ The current pydantic user schema according to the FastAPI Docs
         - Currently not using a hashed password!!!
     Attributes:
-        rz_username:str
         full_name : str
         organisation_unit : str
         has_admin_privileges : bool
     """
-    rz_username: str
     full_name: str
     organisation_unit: str
     has_admin_privileges: bool
     # TODO: Add hashed password Currently not using a password
 
-
-class UserCreate(User):
-    """Additional User subclass for referencing the users password
-    Attributes:
-        hashed_password : str
-    """
-    hashed_password: str
+    class Config:
+        orm_mode = True
