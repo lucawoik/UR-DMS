61081c4c2cf500385471aab3eab627b0f83c4759
Luca Woik <luca@luca-woik.de>
2023-03-23 19:50:31 +0100

    Added read utilities for getting all users and getting a user by their username

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,24 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Method, which gets all the users from the database
+    :param db:
+    :param skip:
+    :param limit:
+    :return:
+    """
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def get_user_by_username(db: Session, rz_username: str):
+    """
+    Get a user from the DB according to the username
+    :param db:
+    :param rz_username:
+    :return:
+    """
+    return db.query(models.User).filter(models.User.rz_username == rz_username).first()
