9a294d0d4e01b5a43cc4cbd7a39ada50ba1eba01
Luca Woik <luca@luca-woik.de>
2023-03-24 17:07:26 +0100

    Added a creat_user method

--- backend/crud.py
+++ backend/crud.py
@@ -22,3 +22,21 @@ def get_user_by_username(db: Session, rz_username: str):
     :return:
     """
     return db.query(models.User).filter(models.User.rz_username == rz_username).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    """
+    Create a new user according to the User model and commit it to the database
+    :param db:
+    :param user:
+    :return:
+    """
+    db_user = models.User(rz_username=user.rz_username,
+                          full_name=user.full_name,
+                          organisation_unit=user.organisation_unit,
+                          has_admin_privileges=user.has_admin_privileges,
+                          hashed_password=user.hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
