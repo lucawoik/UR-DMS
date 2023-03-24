bc2890dece71c8c02ed67f92fecb7051cdf37c78
Luca Woik <luca@luca-woik.de>
2023-03-24 12:56:17 +0100

    Removed unnecessary code, that was used to test login with fake db

--- backend/main.py
+++ backend/main.py
@@ -28,58 +28,6 @@ def get_db():
         db.close()
 
 
-# TODO: For now using a fake-db (dictionary), needs to be replaced with backend.db later
-fake_users_db = {
-    "user": {
-        "rz_username": "user",
-        "full_name": "Max Mustermann",
-        "organisation_unit": "Medieninformatik",
-        "has_admin_privileges": False,
-        "hashed_password": "fakehashed1234"
-    }
-}
-
-
-# TODO: Current implementation according to FastAPI docs
-#  (https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/)
-def fake_decode_token(token):
-    """ This method decodes the token that is given from the authentication process and gets the corresponding
-    user from the database.
-    :param token:
-    :return:
-    """
-    user = get_user(fake_users_db, token)
-    return user
-
-
-def get_user(db, rz_username: str):
-    """ Helper function which gets the user from the fake-db dictionary
-    :param db:
-    :param rz_username:
-    :return:
-    """
-    if rz_username in db:
-        user_dict = db[rz_username]
-        return schemas.UserCreate(**user_dict)
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    """ Dependency, which returns the current user.
-        - oauth2_scheme as sub-dependency
-    :param token: str
-    :return: current_user: User
-    """
-    user = fake_decode_token(token)
-    # Exception code taken from https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
 @app.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
     """
