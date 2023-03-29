cd5a0f439b8232139cb030bbee0a4acfa5d02949
Luca Woik <luca@luca-woik.de>
2023-03-29 12:02:08 +0200

    Repaired schemas.py which got mixed up after merge conflict

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,12 +3,7 @@
 from pydantic import BaseModel
 
 
-<<<<<<< HEAD
-class User(BaseModel):
-=======
 # TODO: Add source? (https://fastapi.tiangolo.com/tutorial/security/get-current-user/#__tabbed_2_1)
-
-
 class User(BaseModel):
     """ The current pydantic user schema according to the FastAPI Docs
         - Currently not using a hashed password!!!
@@ -17,50 +12,10 @@ class User(BaseModel):
         organisation_unit : str
         has_admin_privileges : bool
     """
->>>>>>> origin/main
     rz_username: str
     full_name: str
     organisation_unit: str
     has_admin_privileges: bool
-<<<<<<< HEAD
-    hashed_password: str
-
-
-class Device(BaseModel):
-    device_id: str
-    title: str
-    device_type: str
-    description: str
-    accessories: str
-    rz_username_buyer: str
-    serial_number: str
-    image_url: str
-
-
-class OwnerTransaction(BaseModel):
-    owner_transaction_id: str
-    rz_username: str
-    timestamp_owner_since: str
-    device_id: str
-
-
-class LocationTransaction(BaseModel):
-    location_transaction_id: str
-    room_code: str
-    timestamp_located_since: str
-    device_id: str
-
-
-class PurchasingInformation(BaseModel):
-    purchasing_information_id: str
-    price: str
-    timestamp_warranty_end: str
-    timestamp_purchase: str
-    cost_centre: int
-    seller: str
-    device_id: str
-=======
-    # TODO: Add hashed password Currently not using a password
 
     class Config:
         orm_mode = True
@@ -92,4 +47,3 @@ class TokenData(BaseModel):
         username: str
     """
     rz_username: str | None = None
->>>>>>> origin/main
