b4f5445f4bc84a698026bb90775274990a693a47
Luca Woik <luca@luca-woik.de>
2023-03-31 11:15:25 +0200

    Added login screen component

---
+++ frontend/src/screens/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className={"is-primary"}>Login</h1>
+        </div>
+    )
+}
+
+export default Login;
