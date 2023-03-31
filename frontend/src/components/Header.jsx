ef2f5c1cb13f6366869e586debcc3c2cb7901031
Luca Woik <luca@luca-woik.de>
2023-03-31 14:06:29 +0200

    Added custom logo to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import logo from "../Logo.png"
+
 const Header = () => {
     return (
         <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png"
-                         alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112"
+                <a className="navbar-item" href="#">
+                    <img src={logo}
+                         alt="UR-DMS: Geräteverwaltung an der Uni Regensburg" width="112"
                          height="28" />
                 </a>
 
