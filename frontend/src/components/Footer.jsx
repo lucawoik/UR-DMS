0a965f356cf7e6c17f721292fcea0ca54fa035df
Luca Woik <luca@luca-woik.de>
2023-03-31 13:53:07 +0200

    Changed footer text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,9 +6,13 @@ const Footer = () => {
             <footer className="footer">
                 <div className="content has-text-centered">
                     <p>
-                        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-                        licensed
-                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                        Abschlussprojekt ASE WS22/23 - <strong>Geräteverwaltung</strong> von Luca Woik.
+                        <br/>
+                        {/* TODO: Add Licensing */}
+                        The source code is
+                        licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+                        <br/>
+                        The website content
                         is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                     </p>
                 </div>
