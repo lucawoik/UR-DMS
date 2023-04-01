228273634bb29a23e35af9241113310a9826b5ef
Luca Woik <luca@luca-woik.de>
2023-04-01 20:32:48 +0200

    Created CR(UD) for devices

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -40,3 +42,46 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_devices(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Get all devices from the database.
+    :param db:
+    :param skip:
+    :param limit:
+    :return:
+    """
+    return db.query(models.Device).offset(skip).limit(limit).all()
+
+
+def get_device_by_id(db: Session, device_id: str):
+    """
+    Get a device from the database by id.
+    :param db:
+    :param device_id:
+    :return:
+    """
+    return db.query(models.Device).filter(models.Device.device_id == device_id).first()
+
+
+def create_device(db: Session, device: schemas.DeviceCreate):
+    """
+    Create a device according to the schema DeviceCreate and add it to the database.
+    :param db:
+    :param device:
+    :return:
+    """
+    db_device = models.Device(device_id=str(uuid.uuid4()),
+                              title=device.title,
+                              device_type=device.device_type,
+                              description=device.description,
+                              accessories=device.accessories,
+                              rz_username_buyer=device.rz_username_buyer,
+                              serial_number=device.serial_number,
+                              image_url=device.image_url
+                              )
+    db.add(db_device)
+    db.commit()
+    db.refresh(db_device)
+    return db_device
