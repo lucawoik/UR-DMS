a9cdddbd9a298ddb36b14dc23a9e08af78c5e30c
Luca Woik <luca@luca-woik.de>
2023-04-14 22:34:35 +0200

    Added method to test connection to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Login from './components/Login'
 import AbstractPage from './components/AbstractPage'
@@ -12,6 +12,31 @@ import History from "./components/History";
 const authorized = false;
 
 const App = () => {
+
+    const [message, setMessage] = useState("");
+
+    const getWelcomeMessage = async () => {
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+            },
+        };
+        const response = await fetch("/api", requestOptions);
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.log("No connection to backend could be established.");
+        }
+        else {
+            setMessage(data.message);
+        }
+    };
+
+    useEffect(() => {
+        getWelcomeMessage()
+    }, [])
+
     return (
         <AbstractPage>
             {authorized ? <></> : <Login/>}
