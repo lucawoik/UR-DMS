641c1e282bea52244a22830b3f4076495277682a
Luca Woik <luca@luca-woik.de>
2023-04-14 23:07:11 +0200

    Added Logout button to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
+import {UserContext} from "../context/UserContext";
 
 import logo from "../Logo.png"
 
 const Header = () => {
+    const [token, setToken] = useContext(UserContext);
+
+    const handleLogout = () => {
+        setToken(null);
+    }
     return (
         <nav className="navbar is-primary is-spaced" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
@@ -10,6 +16,19 @@ const Header = () => {
                     <img src={logo}
                          alt="UR-DMS: Geräteverwaltung an der Uni Regensburg" width="112"
                          height="28" />
+                </div>
+            </div>
+            <div className="navbar-menu is-block">
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                    {
+                        token && (
+                            <button className="button" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        )
+                    }
+                    </div>
                 </div>
             </div>
         </nav>
